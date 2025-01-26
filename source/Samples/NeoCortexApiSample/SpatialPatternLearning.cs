a1fd4439095e0a8d240b60394944ed1a5c86b1cf
shahriyar774 <shahriyarshaon6@gmail.com>
2025-01-26 13:32:06 +0100

    Analysis with Double Max Value Configuration: 50

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -53,7 +53,7 @@ namespace NeoCortexApiSample
                 StimulusThreshold=10,
             };
 
-            double max = 100;
+            double max = 50;
 
             //
             // This dictionary defines a set of typical encoder parameters.
