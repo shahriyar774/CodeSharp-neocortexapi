6173eec5d294ea7f272efea086f2abf4ab12a148
salmandewan <salmanhossain18@gmail.com>
2025-02-11 22:17:35 +0100

    elementes adde with cycle 700 for quality prediction

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
             }
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 600;
+            int maxSPLearningCycles = 700;
 
             int numStableCycles = 0;
 
