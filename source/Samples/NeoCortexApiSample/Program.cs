67f49630438b2150fa5c41f26f8af5e075b06ca4
salmandewan <salmanhossain18@gmail.com>
2025-02-13 21:46:05 +0100

    added 3 dimenshinvalue

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -94,9 +94,9 @@ namespace NeoCortexApiSample
             // These list are used to see how the prediction works.
             // Predictor is traversing the list element by element. 
             // By providing more elements to the prediction, the predictor delivers more precise result.
-            var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, 6.0 };
-            var list2 = new double[] { 2.0, 3.0, 4.0, 5.0 };
-            var list3 = new double[] { 8.0, 1.0, 2.0, 3.0 };
+            var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, 6.0, 7.0 };
+            var list2 = new double[] { 2.0, 3.0, 4.0, 5.0, 6.0 };
+            var list3 = new double[] { 8.0, 1.0, 2.0, 3.0, 4.0 };
 
             predictor.Reset();
             PredictNextElement(predictor, list1);
