3cf35ae4716b214e4de5d1cfeef17554b872115e
shahriyar774 <shahriyarshaon6@gmail.com>
2025-02-16 01:22:25 +0100

    Increased inputBits from 200 to 250 for improved encoding resolution

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -28,7 +28,7 @@ namespace NeoCortexApiSample
             double maxBoost = 7.0;
 
             // We will use 200 bits to represent an input vector (pattern).
-            int inputBits = 200;
+            int inputBits = 250;
 
             // We will build a slice of the cortex with the given number of mini-columns
             int numColumns = 1024;
