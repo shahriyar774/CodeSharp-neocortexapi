3400ed8210fc4043037d007b246b8a5fcf8b1b21
salmandewan <salmanhossain18@gmail.com>
2025-02-19 23:11:09 +0100

    added extra element

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -94,7 +94,7 @@ namespace NeoCortexApiSample
             // These list are used to see how the prediction works.
             // Predictor is traversing the list element by element. 
             // By providing more elements to the prediction, the predictor delivers more precise result.
-            var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, 6.0, 7.0 };
+            var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, 6.0, 7.0, 8.0 };
             var list2 = new double[] { 2.0, 3.0, 4.0, 5.0, 6.0 };
             var list3 = new double[] { 8.0, 1.0, 2.0, 3.0, 4.0 };
 
