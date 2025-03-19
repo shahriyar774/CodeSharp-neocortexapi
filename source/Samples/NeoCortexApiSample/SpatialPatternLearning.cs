7b5bc29d89f82235aef5438558ab853b16b31eb3
salmandewan <salmanhossain18@gmail.com>
2025-03-19 19:29:53 +0100

    2d full setup

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
             }
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 810;
+            int maxSPLearningCycles = 820;
 
             int numStableCycles = 0;
 
